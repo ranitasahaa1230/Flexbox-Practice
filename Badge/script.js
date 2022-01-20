5b7d89ecf704eddb90ce384d75ff27760f3ab8ed
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-21 02:31:07 +0530

    [feat] update badge

---
+++ Badge/script.js
@@ -0,0 +1,21 @@
+const button=document.querySelector(".btn2");
+const badge=document.querySelector(".badge4")
+
+// button.addEventListener('click',()=>{
+//     if(badge.style.display==="none"){
+//         badge.style.display="block";
+//     }else{
+//         badge.style.display="none";
+//     }
+// })
+
+
+button.addEventListener('click',()=>{
+    if(badge.dataset.toggle==="0"){
+        badge.style.visibility="hidden";
+        badge.dataset.toggle="1"
+    }else{
+        badge.style.visibility="visible";
+        badge.dataset.toggle="0";
+    }
+})
