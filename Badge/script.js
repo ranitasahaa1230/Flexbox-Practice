b016560ed29a4df9e7a034ace81dd705368d930d
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-22 18:13:03 +0530

    Update script.js

--- Badge/script.js
+++ Badge/script.js
@@ -10,12 +10,27 @@ const badge=document.querySelector(".badge4")
 // })
 
 
-button.addEventListener('click',()=>{
-    if(badge.dataset.toggle==="0"){
-        badge.style.visibility="hidden";
-        badge.dataset.toggle="1"
-    }else{
-        badge.style.visibility="visible";
-        badge.dataset.toggle="0";
-    }
+// button.addEventListener('click',()=>{
+//     if(badge.dataset.toggle==="0"){
+//         badge.style.visibility="hidden";
+//         badge.dataset.toggle="1"
+//     }else{
+//         badge.style.visibility="visible";
+//         badge.dataset.toggle="0";
+//     }
+// })
+
+// button.addEventListener("click",()=>{
+//     const toggleBtn=badge.classList;
+//     if(toggleBtn.contains("toggle")){
+//         toggleBtn.remove("toggle");
+//     }
+//     else{
+//         toggleBtn.add("toggle");
+//     }
+// })
+
+button.addEventListener("click",()=>{
+    badge.classList.toggle('toggle8');
+    
 })
