3ea8eb1563f0c7fbd62e0b598f2aa2f63de1b046
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-23 19:47:50 +0530

    update

---
+++ Header/script.js
@@ -0,0 +1,5 @@
+const stickyContainerList = document.querySelectorAll(".sticky-container");
+const stickyHeaderList = document.querySelectorAll(".standard");
+const disabledList = document.querySelectorAll(".disabled");
+const shortHeader = document.querySelector(".short");
+const shortContainer = document.querySelector(".short-container");
