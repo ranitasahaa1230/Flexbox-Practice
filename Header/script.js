17d51423c9e0a26b17f4ea1c7cea0886150f5fb6
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-23 21:15:58 +0530

    [feat] complete header

--- Header/script.js
+++ Header/script.js
@@ -1,5 +1,18 @@
-const stickyContainerList = document.querySelectorAll(".sticky-container");
-const stickyHeaderList = document.querySelectorAll(".standard");
 const disabledList = document.querySelectorAll(".disabled");
 const shortHeader = document.querySelector(".short");
-const shortContainer = document.querySelector(".short-container");
+const shortContainer = document.querySelector(".div-header");
+
+shortContainer.addEventListener("scroll",(e)=>{
+    console.log("clicked");
+    if(e.target.scrollTop === 0){
+        shortHeader.classList.remove("collapse");
+        disabledList.forEach((item)=>{
+            item.classList.remove("hide")
+        })
+    }else{
+        shortHeader.classList.add("collapse");
+        disabledList.forEach((item)=>{
+            item.classList.add("hide")
+        })  
+      }
+})
