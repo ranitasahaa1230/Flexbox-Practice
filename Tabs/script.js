56facaee3ce815521d5c0eff4ce5ed7d2606a154
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-25 01:14:12 +0530

    fix styles

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,5 +1,5 @@
-const slider = document.querySelector(".slider");
-const tabLinks = document.querySelectorAll(".tab-link");
+// const slider = document.querySelector(".slider");
+// const tabLinks = document.querySelectorAll(".tab-link");
 
 // const getLeftOffset = (element) => element.getBoundingClientRect().left;
 
@@ -14,3 +14,38 @@ const tabLinks = document.querySelectorAll(".tab-link");
 // tabLinks.forEach((tabLink) => {
 //   tabLink.addEventListener("click", handleTabLinkClick);
 // });
+
+// -------------------------------------------
+
+
+const tabBtns = document.querySelectorAll(".tab-link");
+const tabBtnScroll = document.querySelectorAll(".btn-tab-scroll");
+
+const underline = document.querySelector(".slidder");
+const underlineScroll = document.querySelector(".slider-scroll");
+
+tabBtns.forEach((tabs,index)=>{
+    tabs.addEventListener("click",()=>{
+        tabBtns.forEach(tabs=>{
+            tabs.classList.remove("active");
+        })
+        tabs.classList.add("active");
+        underline.style.left = `calc(calc(100% / 4) * ${index})`;
+
+    })
+})
+
+tabBtnScroll.forEach((tabs, index) => {
+    tabs.addEventListener("click", function showActiveTab() {
+      tabBtnScroll.forEach((tabs) => {
+        tabs.classList.remove("active");
+      });
+  
+      tabs.classList.add("active");
+  
+      tabs.scrollIntoView();
+  
+      underlineScroll.style.left = `calc(100px * ${index})`;
+    });
+  });
+  
