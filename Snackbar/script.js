51ef02df79f4aa9b21e3f7a846222939fd3f5f1e
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-25 12:58:46 +0530

    [feat] complete snackbar

--- Snackbar/script.js
+++ Snackbar/script.js
@@ -1,5 +1,47 @@
-const baseline=document.querySelector(".baseline");
-const leading=document.querySelector(".leading");
-const stacked=document.querySelector(".stacked");
+const baseline = document.querySelector("#btn-baseline");
+const leading = document.querySelector("#btn-leading");
+const stacked = document.querySelector("#btn-stacked");
+const close = document.querySelectorAll(".close-icon");
+const baselineSnackbar = document.querySelector(".baseline");
+const leadingSnackbar = document.querySelector(".leading");
+const stackedSnackbar = document.querySelector(".stacked");
+const loopSnackbar = document.querySelector(".loop");
 
-baseline.addEventListener("click",)
+
+// baseline.addEventListener("click",()=>{
+//     baselineSnackbar.style.display="flex";
+//     setTimeout(()=>{
+//         baselineSnackbar.style.display="none";
+//     },4000);
+// });
+baseline.addEventListener("click",()=>{
+    loopSnackbar.classList.add("show");
+        baselineSnackbar.style.display="flex";
+});
+
+leading.addEventListener("click",()=>{
+    loopSnackbar.classList.add("show");
+        leadingSnackbar.style.display="flex";
+});
+
+stacked.addEventListener("click",()=>{
+    loopSnackbar.classList.add("show");
+    stackedSnackbar.style.display="flex";
+});
+
+close.forEach((closeBtn) => {
+    closeBtn.addEventListener("click", (e) => {
+      let parent = closeBtn.closest("div").className;
+      switch (parent) {
+        case "baseline":
+          baselineSnackbar.style.display = "none";
+          break;
+        case "leading":
+          leadingSnackbar.style.display = "none";
+          break;
+        case "stacked":
+          stackedSnackbar.style.display = "none";
+          break;
+      }
+    });
+  });
