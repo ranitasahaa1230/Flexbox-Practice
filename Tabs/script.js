ef43617462e2522ed7c742ee4d5d72915902ee63
Ranita Saha <saharanitaa1230@gmail.com>
2022-01-25 14:04:04 +0530

    fix tabs

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,21 +1,21 @@
-// const slider = document.querySelector(".slider");
-// const tabLinks = document.querySelectorAll(".tab-link");
+// // const slider = document.querySelector(".slider");
+// // const tabLinks = document.querySelectorAll(".tab-link");
 
-// const getLeftOffset = (element) => element.getBoundingClientRect().left;
+// // const getLeftOffset = (element) => element.getBoundingClientRect().left;
 
-// const handleTabLinkClick = (event) => {
-//   const clickedElementLeft = getLeftOffset(event.target);
+// // const handleTabLinkClick = (event) => {
+// //   const clickedElementLeft = getLeftOffset(event.target);
 
-//   const clickedElementParentLeft = getLeftOffset(event.target.parentNode);
+// //   const clickedElementParentLeft = getLeftOffset(event.target.parentNode);
 
-//   slider.style.left = `${clickedElementLeft - clickedElementParentLeft}px`;
-// };
+// //   slider.style.left = `${clickedElementLeft - clickedElementParentLeft}px`;
+// // };
 
-// tabLinks.forEach((tabLink) => {
-//   tabLink.addEventListener("click", handleTabLinkClick);
-// });
+// // tabLinks.forEach((tabLink) => {
+// //   tabLink.addEventListener("click", handleTabLinkClick);
+// // });
 
-// -------------------------------------------
+// // -------------------------------------------
 
 
 const tabBtns = document.querySelectorAll(".tab-link");
